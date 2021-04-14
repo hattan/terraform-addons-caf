323208ea235bc409aa93e360740eb06dbcde080f
polatengin <enpolat@microsoft.com>
2021-04-14 16:46:39 +0300

    locals file for remote state is created

---
+++ examples/locals.remote_tfstates.tf
@@ -0,0 +1,35 @@
+data "terraform_remote_state" "remote" {
+  for_each = try(var.landingzone.tfstates, {})
+
+  backend = var.landingzone.backend_type
+  config = {
+    storage_account_name = local.landingzone[try(each.value.level, "current")].storage_account_name
+    container_name       = local.landingzone[try(each.value.level, "current")].container_name
+    resource_group_name  = local.landingzone[try(each.value.level, "current")].resource_group_name
+    subscription_id      = var.tfstate_subscription_id
+    key                  = each.value.tfstate
+  }
+}
+
+locals {
+  tags = merge(var.tags, { "rover_version" = var.rover_version })
+
+  landingzone = {
+    current = {
+      storage_account_name = var.tfstate_storage_account_name
+      container_name       = var.tfstate_container_name
+      resource_group_name  = var.tfstate_resource_group_name
+    }
+    lower = {
+      storage_account_name = var.lower_storage_account_name
+      container_name       = var.lower_container_name
+      resource_group_name  = var.lower_resource_group_name
+    }
+  }
+
+  remote = {
+    keyvaults = {
+      for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.keyvaults[key], {}))
+    }
+  }
+}
