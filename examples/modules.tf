57690997068c16f7faee1d077a6ceaeba5b61582
polatengin <enpolat@microsoft.com>
2021-04-14 16:39:33 +0300

    azure devops module added to module variables list

--- examples/modules.tf
+++ examples/modules.tf
@@ -3,6 +3,7 @@ module "caf" {
 
   global_settings                = var.global_settings
   gitlab_projects                = var.gitlab_projects
+  azure_devops_projects          = var.azure_devops_projects
   current_landingzone_key        = var.landingzone.key
   tenant_id                      = var.tenant_id
   logged_user_objectId           = var.logged_user_objectId
