ebc50db2d6720320021112b539df10e9f73946e1
polatengin <enpolat@microsoft.com>
2021-04-14 16:42:05 +0300

    document formatted

--- main.tf
+++ main.tf
@@ -22,7 +22,6 @@ data "azurerm_client_config" "current" {}
 # 2 - deploying a landing zone or a solution from vscode
 #  Step 1 has been executed. The rover is still connect to a logged in user azure session. The rover use the user's credentials to connect the default azure subscription to identity the storage account and the keyvault holding the tfstate and the launchpad_app_level0 credentials. The rover set the terraform ARM_* variables to change the terraform provider Azure context (client id, secret, tenant and subscription). The logged_aad_app_objectId is set to the launchpad_app_level0's client_id. Note in that scenario the azure session does not change. Meaning when terraform execute some local execution scripts they are executed in the context of the logged_in_user and not the azure ad application. To simulate from vscode the execution of a local exec with the launchpad_app_level0 credentials, the rover must be executed with the parameter --impersonate (cannot be used during the launchpad initial deployment and destruciton)
 
-
 data "azuread_service_principal" "logged_in_app" {
   count          = try(data.azurerm_client_config.current.object_id, null) == null ? 1 : 0
   application_id = data.azurerm_client_config.current.client_id
